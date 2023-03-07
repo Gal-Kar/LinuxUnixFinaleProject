37a70cc12a317873149246dd3f569ec5666c2986
Gal-Kar <galkarasente@gmail.com>
2023-03-07 12:40:39 +0200

    fix

--- perl_scripts/mkdir.pl
+++ perl_scripts/mkdir.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 
-my $dir = "/path/to/new/directory";
-
+my $dir = "$ARGV[0]";
 unless (-d $dir) { # check if directory already exists
     if(mkdir $dir) {
         print "Directory created successfully.\n";
