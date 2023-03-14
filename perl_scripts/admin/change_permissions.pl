5dfc182c8a6f0976dae9e31bcb1788dc9b826e15
Gal-Kar <galkarasente@gmail.com>
2023-03-14 19:36:43 +0200

    a

--- perl_scripts/admin/change_permissions.pl
+++ perl_scripts/admin/change_permissions.pl
@@ -1,20 +1,30 @@
-use User::pwent;
+use strict;
+use warnings;
 
 my $username = "exampleusername";
 my $groupname = "examplegroup";
 
-my $user = getpwnam($username);
+# Open the /etc/group file for reading and writing
+open my $group_file, "+<", "/etc/group" or die "Cannot open /etc/group: $!";
 
-# Extract the existing group IDs for the user
-my @group_ids = split(',', $user->groups);
-
-# Check if the user is already a member of the group
-my $already_member = grep { $_ eq $groupname } @group_ids;
-
-# If the user is not already a member of the group, add the group ID to the list
-if (!$already_member) {
-    push(@group_ids, $groupname);
-    my $new_groups = join(',', @group_ids);
-    $user->groups($new_groups);
-    $user->passwd;  # required to save the changes
+# Read each line of the file and look for the group we want to modify
+while (my $line = <$group_file>) {
+    chomp $line;
+    my ($name, $password, $gid, $users) = split /:/, $line;
+    
+    # If we found the group we want to modify, add the user to the list of users
+    if ($name eq $groupname) {
+        my @user_list = split /,/, $users;
+        push @user_list, $username;
+        my $new_users = join(',', @user_list);
+        
+        # Seek back to the beginning of the line and write the modified line to the file
+        seek $group_file, -length($line), 1;
+        print $group_file join(':', $name, $password, $gid, $new_users), "\n";
+        
+        last;  # Exit the loop once we've modified the group
+    }
 }
+
+# Close the /etc/group file
+close $group_file;
